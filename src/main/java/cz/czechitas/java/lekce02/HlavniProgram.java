f7edef5960bd34e1928f025bdbc616b1e90d2f8e
rempa359 <rempa@seznam.cz>
2023-02-14 20:21:13 +0100

    Vesnice a prasatko

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -6,9 +6,84 @@ public class HlavniProgram {
     private Turtle zofka;
     public void start() {
         zofka = new Turtle();
+        zofka.setLocation(250,250);
+        for (int i = 0; i < 10; i++) {
+            nakresliDomek();
+            zofka.penUp();
+            zofka.turnRight(90);
+            zofka.move(150);
+            zofka.turnLeft(90);
+            zofka.penDown();
 
-        //TODO tady bude kód
+        }
+
+        zofka.setLocation(600,600);
+
+        prasatko();
     }
+
+    private void prasatko() {
+        zofka.turnLeft(90);
+        nakresliDomek();
+        zofka.turnLeft(90);
+        zofka.move(100);
+
+        nozicky();
+
+        zofka.move(150);
+        zofka.turnLeft(90);
+
+        nozicky();
+
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnLeft(45);
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(50);
+        zofka.turnLeft(45);
+        zofka.move(100);
+        zofka.turnRight(180);
+    }
+
+    private void nozicky() {
+        zofka.turnRight(45);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnLeft(45);
+    }
+
+    private void nakresliDomek() {
+        nakresliObdelnik();
+        zofka.move(150);
+        zofka.turnLeft(30);
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        zofka.turnLeft(30);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+    }
+
+    public void nakresliObdelnik() {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(150);
+            zofka.turnLeft(90);
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+    }
+
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
